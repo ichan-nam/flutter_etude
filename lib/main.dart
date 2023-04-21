12a81572edb43c398248014e1228a054e6cafe0e
Timo Nam <dwbh7358@gmail.com>
2023-04-21 21:55:43 +0900

    feat: Portrait 모드 고정 ✨

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
+  // runApp() 실행 전 Engine 레이어 Platform Channels를 사용할 때 호출
+  // 당장 위젯을 그리진 않고 binding만 함
+  // runApp()은 첫 줄에서 WidgetsFlutterBinding.ensureInitialized() 호출
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Portrait 모드 고정
+  // 얘가 Platform Channels를 사용하는 녀석
+  // Platform Channels를 사용하는 메소드는 다 비동기
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(const MyApp());
 }
 
