baf30441e3621070a4882fff59d6bc9f5ba76a53
Timo Nam <dwbh7358@gmail.com>
2023-04-24 23:34:07 +0900

    feat: BottomNavigationBar 구현 ✨

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({
     super.key,
   });
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static const _widgetOptions = <Widget>[
+    Text('LIST'),
+    Text('MEETING'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('가상인맥'),
+      ),
+      body: Center(
+        child: _widgetOptions[_selectedIndex],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
@@ -22,6 +43,8 @@ class HomeScreen extends StatelessWidget {
             label: 'meeting',
           ),
         ],
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
       ),
       backgroundColor: Theme.of(context).primaryColor,
     );
