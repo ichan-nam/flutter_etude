ce5c0c832a2a988b70bf3ca73bec24708d8865ba
Timo Nam <dwbh7358@gmail.com>
2023-05-16 00:48:26 +0900

    fix: 안드로이드에서 HTTPS 통신 시 CERTIFICATE_VERIFY_FAILED 에러 대응 🐛

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_etude/screens/home_screen.dart';
 import 'package:flutter_etude/extensions/color_extension.dart';
 
 void main() async {
+  // 안드로이드에서 HTTPS 통신 시 CERTIFICATE_VERIFY_FAILED 에러를 피하기 위한 코드
+  // 배포할 때는 정상적인 인증서를 등록해야 함
+  HttpOverrides.global = MyHttpOverrides();
+
   // runApp() 실행 전 Engine 레이어 Platform Channels를 사용할 때 호출
   // 당장 위젯을 그리진 않고 binding만 함
   // runApp()은 첫 줄에서 WidgetsFlutterBinding.ensureInitialized() 호출
@@ -40,5 +46,16 @@ class MyApp extends StatelessWidget {
             const ProgressIndicatorThemeData(color: CustomColors.customWhite),
       ),
     );
+  }
+}
+
+// 안드로이드에서 HTTPS 통신 시 CERTIFICATE_VERIFY_FAILED 에러를 피하기 위한 코드
+// 배포할 때는 정상적인 인증서를 등록해야 함
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
   }
 }
